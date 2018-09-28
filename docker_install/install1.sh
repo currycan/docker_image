c715132fac93f12cc5bfef134cbc2ed0d2059dbf
currycan <ansandy@foxmail.com>
2018-09-28 22:46:19 +0800

    Update install1.sh

--- docker_install/install1.sh
+++ docker_install/install1.sh
@@ -1,6 +1,24 @@
 #!/bin/sh
 set -e
 
+# This script is meant for quick & easy install via:
+#   $ curl -fsSL get.docker.com -o get-docker.sh
+#   $ sh get-docker.sh
+#
+# For test builds (ie. release candidates):
+#   $ curl -fsSL test.docker.com -o test-docker.sh
+#   $ sh test-docker.sh
+#
+# NOTE: Make sure to verify the contents of the script
+#       you downloaded matches the contents of install.sh
+#       located at https://github.com/docker/docker-install
+#       before executing.
+#
+# Git commit from https://github.com/docker/docker-install when
+# the script was uploaded (Should only be modified by upload job):
+SCRIPT_COMMIT_SHA=36b78b2
+
+
 # This value will automatically get changed for:
 #   * edge
 #   * test
