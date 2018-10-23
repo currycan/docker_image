8e84ee0b9d7e64787bac2df3c93075f491c25911
currycan <ansandy@foxmail.com>
2018-10-23 11:06:30 +0800

    Update install.sh

--- docker_install/install.sh
+++ docker_install/install.sh
@@ -45,6 +45,8 @@ EOL
     "max-concurrent-downloads": 10,
     "log-driver": "json-file",
     "log-level": "warn",
+    "metrics-addr" : "127.0.0.1:9323",
+    "experimental" : true
     "log-opts": {
        "max-size": "10m",
        "max-file": "3"
